5d27d8cad88696f03f8c81491ed53083167e035e
Caio Lucio dos Santos Almeida <241.14.035@EDU.gruposete.local>
2026-03-04 22:08:43 -0300

    Teste

---
+++ components/Resultado.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View } from "react-native"
+
+interface ResultadoProps {
+    imc?: number;
+    classificacao?: string;
+}
+
+export function Resultado({ imc, classificacao }: ResultadoProps) {
+    return (
+        <View style={styles.resultado}>
+            <Text style={styles.labelResultado}>Seu IMC:</Text>
+            <Text style={styles.imc}>{imc?.toFixed(2)}</Text>
+            <Text style={styles.labelResultado}>Classificação</Text>
+            <Text style={styles.classificacao}>{classificacao}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create(
+    {
+        resultado: {
+            backgroundColor: '#EEE',
+            padding: 20,
+            borderRadius: 20
+        },
+        labelResultado: {
+            textAlign: 'center',
+            fontSize: 18,
+            marginBottom: 10
+        },
+        imc: {
+            backgroundColor: '#FFF',
+            textAlign: 'center',
+            padding: 10,
+            fontSize: 24,
+            borderRadius: 10,
+            marginBottom: 10
+        },
+        classificacao: {
+            backgroundColor: '#F00',
+            color: '#FFF',
+            textAlign: 'center',
+            padding: 10,
+            fontSize: 24,
+            borderRadius: 10,
+            marginBottom: 10
+        }
+    }
+)
